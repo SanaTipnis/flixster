07c6c8f5672e628cb0e7dc38c6e9349a6be1f53d
Sana Tipnis <sat5652@psu.edu>
2021-02-27 11:23:13 -0500

    first attempt to add to part 2 of project

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Sana Tipnis on 2/23/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
